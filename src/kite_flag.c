a800943c6c96516232cfca79bfb60d579da51f1c
Rasmus Stougaard <rasmus.stougaard@gmail.com>
2013-10-07 20:56:38 +0200

    Added current flag signalling LEDs

--- src/kite_flag.c
+++ src/kite_flag.c
@@ -31,7 +31,7 @@
 // CONFIGURATION Section =========================
 
 // uncomment this line to single step in simuino simulator
-//#define DEBUG_STATE 1
+#define DEBUG_STATE 1
 
 //variabel for changing heat_time in min
 const int TRANSITION_TIME_MINUTES = 2; 
@@ -56,9 +56,16 @@ const long CYCLE_DURATION = TRANSITION_TIME_MS + HEAT_TIME_MINUTES;
 
 //Output for motor setup,Horn, stop switchs
 const int FLAG_MOTOR = 13;
-const int DEBUG_FLAG_RED = 8;
-const int DEBUG_FLAG_GREEN = 7;
-const int DEBUG_FLAG_YELLOW = 6;
+
+#ifdef DEBUG_STATE
+const int LED_FLAG_RED = 8;
+const int LED_FLAG_GREEN = 7;
+const int LED_FLAG_YELLOW = 6;
+#else
+const int LED_FLAG_RED = A1;
+const int LED_FLAG_GREEN = A2;
+const int LED_FLAG_YELLOW = A3;
+#endif
 
 const int NO_HEAT_FLAG = 2; //red flag
 const int HEAT_IN_PROGESS_FLAG = 3; // green flag
@@ -71,30 +78,25 @@ long epoch;
 void showFlag(int desiredFlag) {
 	int state = digitalRead(desiredFlag);
 
-#ifdef DEBUG_STATE
-    // Serial.print("state: ");
-    // Serial.println(state);
-    // Serial.print("desiredFlag: ");
-    // Serial.println(desiredFlag);
-	// switch off all leds
-	digitalWrite(DEBUG_FLAG_RED, 0);
-	digitalWrite(DEBUG_FLAG_GREEN, 0);
-	digitalWrite(DEBUG_FLAG_YELLOW, 0);
-
 	switch(desiredFlag) {
 		case NO_HEAT_FLAG:
-			digitalWrite(DEBUG_FLAG_RED, 1);
+		digitalWrite(LED_FLAG_RED, HIGH);
+		digitalWrite(LED_FLAG_GREEN, LOW);
+		digitalWrite(LED_FLAG_YELLOW, LOW);
 		break;
 		case HEAT_IN_PROGESS_FLAG:
-			digitalWrite(DEBUG_FLAG_GREEN, 1);
+		digitalWrite(LED_FLAG_RED, LOW);
+		digitalWrite(LED_FLAG_GREEN, HIGH);
+		digitalWrite(LED_FLAG_YELLOW, LOW);
 		break;
 		case HEAT_ENDING_SOON_FLAG:
-			digitalWrite(DEBUG_FLAG_YELLOW, 1);
+		digitalWrite(LED_FLAG_RED, LOW);
+		digitalWrite(LED_FLAG_GREEN, LOW);
+		digitalWrite(LED_FLAG_YELLOW, HIGH);
 		break;
 		default:
 		break;
 	}
-#endif
 
 	if(!state) {
 #ifdef DEBUG_STATE
@@ -119,12 +121,12 @@ void setup()
 	pinMode (HEAT_ENDING_SOON_FLAG, INPUT);  //set pin 12 as input
 	pinMode (FLAG_MOTOR, OUTPUT); //set pin 13 as output
 
-#ifdef DEBUG_STATE
-	// setup some debug pins - could be useful if LEDs where connected here.
-	pinMode(DEBUG_FLAG_RED, OUTPUT);
-	pinMode(DEBUG_FLAG_GREEN, OUTPUT);
-	pinMode(DEBUG_FLAG_YELLOW, OUTPUT);
+	// setup LEDs for signalling current flag
+	pinMode(LED_FLAG_RED, OUTPUT);
+	pinMode(LED_FLAG_GREEN, OUTPUT);
+	pinMode(LED_FLAG_YELLOW, OUTPUT);
 
+#ifdef DEBUG_STATE
 	Serial.begin(9600);
 #endif
 }
@@ -155,37 +157,37 @@ loop:
 	show green for HEAT_TIME minutes
 	show yellow for (HEAT_TIME_MINUTES - HEAT_ENDING_SOON_TIME_MINUTES) minutes
 */
-void loop()
-{
-	long cycle_time_ms = getCycleTime();
+	void loop()
+	{
+		long cycle_time_ms = getCycleTime();
 
 #ifdef DEBUG_STATE
-	Serial.print("cycle time: ");
-	long cycle_time_minutes = cycle_time_ms / MS_PER_MINUTE;
-	Serial.println(cycle_time_minutes);
+		Serial.print("cycle time: ");
+		long cycle_time_minutes = cycle_time_ms / MS_PER_MINUTE;
+		Serial.println(cycle_time_minutes);
 #endif
 
 	// no heat in progress
-	if (cycle_time_ms < TRANSITION_TIME_MS) {
+		if (cycle_time_ms < TRANSITION_TIME_MS) {
 #ifdef DEBUG_STATE
-		Serial.println("show NO_HEAT_FLAG");
+			Serial.println("show NO_HEAT_FLAG");
 #endif
-		showFlag(NO_HEAT_FLAG);
-	} else {
-		// HEAT_ENDING for heat ending
-		if (cycle_time_ms < (TRANSITION_TIME_MS + HEAT_TIME_MS - HEAT_ENDING_SOON_TIME_MS)) {
-#ifdef DEBUG_STATE
-			Serial.println("show HEAT_IN_PROGESS_FLAG");
-#endif
-			showFlag(HEAT_IN_PROGESS_FLAG);
+			showFlag(NO_HEAT_FLAG);
 		} else {
+		// HEAT_ENDING for heat ending
+			if (cycle_time_ms < (TRANSITION_TIME_MS + HEAT_TIME_MS - HEAT_ENDING_SOON_TIME_MS)) {
 #ifdef DEBUG_STATE
-			Serial.println("show HEAT_ENDING_SOON_FLAG");
+				Serial.println("show HEAT_IN_PROGESS_FLAG");
 #endif
-			showFlag(HEAT_ENDING_SOON_FLAG);
+				showFlag(HEAT_IN_PROGESS_FLAG);
+			} else {
+#ifdef DEBUG_STATE
+				Serial.println("show HEAT_ENDING_SOON_FLAG");
+#endif
+				showFlag(HEAT_ENDING_SOON_FLAG);
+			}
+			
 		}
-		
+
+
 	}
-
-
-}
